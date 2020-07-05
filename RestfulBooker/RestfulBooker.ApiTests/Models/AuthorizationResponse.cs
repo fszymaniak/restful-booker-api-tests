169732422bd5fcc2dab4d6a133a98d96175c3ad3
fszymaniak <szymaniak.filip@gmail.com>
2020-07-05 22:12:40 +0200

    Add models folder with some initial files

---
+++ RestfulBooker/RestfulBooker.ApiTests/Models/AuthorizationResponse.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace RestfulBooker.ApiTests.Models.ResponseModel
+{
+    public class AuthorizationResponse
+    {
+        [JsonPropertyName("token")]
+        public string Token { get; set; }
+
+        [JsonPropertyName("reason")]
+        public string Reason { get; set; }
+    }
+}
---
+++ RestfulBooker/RestfulBooker.ApiTests/Models/Booking.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace RestfulBooker.ApiTests.Models
+{
+    public class Booking
+    {
+        [JsonPropertyName("firstname")]
+        public string FirstName { get; set; }
+
+        [JsonPropertyName("lastname")]
+        public string LastName { get; set; }
+
+        [JsonPropertyName("totalprice")]
+        public int TotalPrice { get; set; }
+
+        [JsonPropertyName("depositpaid")]
+        public bool DepositPaid { get; set; }
+
+        [JsonPropertyName("bookingdates")]
+        public BookingDates BookinDates { get; set; }
+
+        [JsonPropertyName("additionalneeds")]
+        public string AdditionalNeeds { get; set; }
+    }
+}
