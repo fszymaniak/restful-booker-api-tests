6eb91e4c3f4464d982d158f9f716dae8d1c4e2e7
fszymaniak <szymaniak.filip@gmail.com>
2020-07-13 18:38:28 +0200

    Add BookingTestBase

---
+++ RestfulBooker/RestfulBooker.ApiTests/BookingTestBase.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Http.Connections;
+using RestfulBooker.ApiTests.Constants;
+using RestfulBooker.ApiTests.Models;
+using RestSharp;
+using System.Text.Json;
+
+namespace RestfulBooker.ApiTests
+{
+    public static class BookingTestBase
+    {
+        public static BookingResponse CreateBooking(string firstName, string lastName, int totalPrice, bool depositPaid, string checkIn, string checkOut, string additionalNeeds)
+        {
+            var bookingDates = new BookingDates
+            {
+                CheckIn = checkIn,
+                CheckOut = checkOut
+            };
+
+            var bookingRequest = new BookingModel
+            {
+                FirstName = firstName,
+                LastName = lastName,
+                TotalPrice = totalPrice,
+                DepositPaid = depositPaid,
+                BookinDates = bookingDates,
+                AdditionalNeeds = additionalNeeds
+            };
+
+            var json = JsonSerializer.Serialize(bookingRequest);
+
+            var client = new RestClient(ApiTestBase.RestfulBokerUrl);
+
+            var request = new RestRequest(Endpoints.BookingEndpoint, Method.POST);
+            request.AddHeaders();
+            request.AddParameter("application/json", json, ParameterType.RequestBody);
+            var response = client.Execute<BookingResponse>(request);
+            var result = JsonSerializer.Deserialize<BookingResponse>(response.Content);
+
+            return result;
+        }
+
+        public static BookingModel GetBookingById(int bookingId)
+        {
+            var client = new RestClient(ApiTestBase.RestfulBokerUrl);
+
+            var request = new RestRequest(Endpoints.GetBookingByIdEndpoint, Method.GET);
+            request.AddUrlSegment(Endpoints.GetBookingByIdSegment, bookingId);
+            request.AddHeaders();
+
+            var response = client.Execute<BookingResponse>(request);
+            var result = JsonSerializer.Deserialize<BookingModel>(response.Content);
+
+            return result;
+        }
+    }
+}
