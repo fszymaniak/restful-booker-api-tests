6439839ffb25d58e3974049fdb474aab84e3225e
fszymaniak <szymaniak.filip@gmail.com>
2020-07-13 18:39:29 +0200

    Add new constant booking and get booking by id endpoints

--- RestfulBooker/RestfulBooker.ApiTests/Constants/Endpoints.cs
+++ RestfulBooker/RestfulBooker.ApiTests/Constants/Endpoints.cs
@@ -8,5 +8,11 @@ namespace RestfulBooker.ApiTests.Constants
     public static class Endpoints
     {
         public static string AuthorizationEndpoint => "/auth";
+
+        public static string BookingEndpoint => "/booking";
+
+        public static string GetBookingByIdEndpoint => "/booking/{bookingId}";
+
+        public static string GetBookingByIdSegment => "bookingId";
     }
 }
