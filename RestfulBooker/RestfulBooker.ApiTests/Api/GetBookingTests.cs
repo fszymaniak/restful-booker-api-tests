4bcb7b003d96dde2f56f18cd8bea83b06a373c3b
fszymaniak <szymaniak.filip@gmail.com>
2020-07-13 19:04:57 +0200

    Add GetBookingTests.cs with one working test

---
+++ RestfulBooker/RestfulBooker.ApiTests/Api/GetBookingTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using RestfulBooker.ApiTests.Extensions;
+using System.Threading.Tasks;
+
+namespace RestfulBooker.ApiTests.Api
+{
+    public class GetBookingTests
+    {
+        [Test]
+        public async Task Get_Booking_WhenIdExists()
+        {
+            // given
+            var booking = BookingTestBase.CreateBooking("Phil", "Collins", 1000, true, "2020-08-23", "2020-08-30", "Breakfasts");
+
+            // when
+            var results = BookingTestBase.GetBookingById(booking.BookingId);
+
+            // then
+            results.ShouldBeValid(booking);
+        }
+    }
+}
