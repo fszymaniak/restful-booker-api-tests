1bd232befc1dc7800ac8503719cbf68c6b5c497d
fszymaniak <szymaniak.filip@gmail.com>
2020-07-13 18:34:38 +0200

    Rename Booking to BookingModel

--- RestfulBooker/RestfulBooker.ApiTests/Models/BookingModel.cs
+++ RestfulBooker/RestfulBooker.ApiTests/Models/BookingModel.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text.Json.Serialization;
-using System.Threading.Tasks;
+﻿using System.Text.Json.Serialization;
 
 namespace RestfulBooker.ApiTests.Models
 {
-    public class Booking
+    public class BookingModel
     {
         [JsonPropertyName("firstname")]
         public string FirstName { get; set; }
