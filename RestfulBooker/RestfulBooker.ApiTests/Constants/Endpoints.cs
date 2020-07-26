ec3a390f294102c5006ea0b0ef555214c3fba3e5
fszymaniak <szymaniak.filip@gmail.com>
2020-07-26 11:19:28 +0200

    Add segments to filtering by name and booking dates in the Constants Endpoints

--- RestfulBooker/RestfulBooker.ApiTests/Constants/Endpoints.cs
+++ RestfulBooker/RestfulBooker.ApiTests/Constants/Endpoints.cs
@@ -14,5 +14,13 @@ namespace RestfulBooker.ApiTests.Constants
         public static string GetBookingByIdEndpoint => "/booking/{bookingId}";
 
         public static string GetBookingByIdSegment => "bookingId";
+
+        public static string GetBookingByFirstNameSegment => "firstname";
+
+        public static string GetBookingByLastNameSegment => "lastname";
+
+        public static string GetBookingByCheckinSegment => "checkin";
+
+        public static string GetBookingByCheckoutSegment => "checkout";
     }
 }
