460331fa146286fe291b819799b27b013b1b3ff5
fszymaniak <szymaniak.filip@gmail.com>
2020-07-26 11:17:19 +0200

    Add methods to filter booking by name or booking dates. Add delete method to clearing up test data.

--- RestfulBooker/RestfulBooker.ApiTests/BookingTestBase.cs
+++ RestfulBooker/RestfulBooker.ApiTests/BookingTestBase.cs
@@ -4,6 +4,7 @@ using RestfulBooker.ApiTests.Models;
 using RestSharp;
 using System.Text.Json;
 using System.Threading.Tasks;
+using RestfulBooker.ApiTests.Models.Responses;
 
 namespace RestfulBooker.ApiTests
 {
@@ -39,12 +40,19 @@ namespace RestfulBooker.ApiTests
 
         public async Task<BookingModel> GetBookingById(int bookingId)
         {
-            var request = GetBookingByIdRequest(bookingId);
+            var request = BookingByIdRequest(bookingId, Method.GET);
 
             var response = await _client.ExecuteAsync<BookingResponse>(request);
             var result = JsonSerializer.Deserialize<BookingModel>(response.Content);
 
             return result;
+        }
+
+        public async Task DeleteBookingById(int bookingId)
+        {
+            var request = BookingByIdRequest(bookingId, Method.DELETE);
+
+            await _client.ExecuteAsync<BookingResponse>(request);
         }
 
         public async Task<IEnumerable<BookingResponse>> GetBookingIds()
@@ -57,11 +65,32 @@ namespace RestfulBooker.ApiTests
             return result;
         }
 
-        public RestRequest GetBookingByIdRequest(int bookingId)
+        public async Task<IEnumerable<BookingIdsResponse>> GetBookingIdsByFirstAndLastName(string firstName, string lastName)
         {
-            var request = new RestRequest(Endpoints.GetBookingByIdEndpoint, Method.GET);
+            var request = GetBookingByFirstAndLastNameRequest(firstName, lastName);
+
+            var response = await _client.ExecuteAsync<BookingIdsResponse>(request);
+            var result = JsonSerializer.Deserialize<IEnumerable<BookingIdsResponse>>(response.Content);
+
+            return result;
+        }
+
+        public async Task<IEnumerable<BookingIdsResponse>> GetBookingIdsByCheckinAndCheckout(string checkin, string checkout)
+        {
+            var request = GetBookingByCheckinAndCheckoutRequest(checkin, checkout);
+
+            var response = await _client.ExecuteAsync<BookingIdsResponse>(request);
+            var result = JsonSerializer.Deserialize<IEnumerable<BookingIdsResponse>>(response.Content);
+
+            return result;
+        }
+
+        public RestRequest BookingByIdRequest(int bookingId, Method method)
+        {
+            var request = new RestRequest(Endpoints.GetBookingByIdEndpoint, method);
             request.AddUrlSegment(Endpoints.GetBookingByIdSegment, bookingId);
             request.AddHeaders();
+            request.AddAuthorizationHeader();
 
             return request;
         }
@@ -73,6 +102,26 @@ namespace RestfulBooker.ApiTests
             var request = new RestRequest(Endpoints.BookingEndpoint, Method.POST);
             request.AddHeaders();
             request.AddParameter("application/json", jsonRequest, ParameterType.RequestBody);
+
+            return request;
+        }
+
+        public RestRequest GetBookingByFirstAndLastNameRequest(string firstName, string lastName)
+        {
+            var request = new RestRequest(Endpoints.BookingEndpoint, Method.GET);
+            request.AddQueryParameter(Endpoints.GetBookingByFirstNameSegment, firstName);
+            request.AddQueryParameter(Endpoints.GetBookingByLastNameSegment, lastName);
+            request.AddHeaders();
+
+            return request;
+        }
+
+        public RestRequest GetBookingByCheckinAndCheckoutRequest(string checkin, string checkout)
+        {
+            var request = new RestRequest(Endpoints.BookingEndpoint, Method.GET);
+            request.AddQueryParameter(Endpoints.GetBookingByCheckinSegment, checkin);
+            request.AddQueryParameter(Endpoints.GetBookingByCheckoutSegment, checkout);
+            request.AddHeaders();
 
             return request;
         }
