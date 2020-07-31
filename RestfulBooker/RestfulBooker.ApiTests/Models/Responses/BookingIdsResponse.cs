068457c865bcae762215ef093079ad233e3ef839
fszymaniak <szymaniak.filip@gmail.com>
2020-07-31 21:35:50 +0200

    Add BookingIdResponse.cs

---
+++ RestfulBooker/RestfulBooker.ApiTests/Models/Responses/BookingIdsResponse.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Serialization;
+
+namespace RestfulBooker.ApiTests.Models.Responses
+{
+    public class BookingIdsResponse
+    {
+        [JsonPropertyName("bookingid")]
+        public int BookingId { get; set; }
+    }
+}
