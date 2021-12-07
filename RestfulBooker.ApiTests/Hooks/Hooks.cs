8402aa84a444c619efefd732a59189d400e4e09c
fszymaniak <szymaniak.filip@gmail.com>
2021-12-08 00:49:57 +0100

    Hooks cleanup

--- RestfulBooker.ApiTests/Hooks/Hooks.cs
+++ RestfulBooker.ApiTests/Hooks/Hooks.cs
@@ -1,11 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using RestfulBooker.ApiTests.Constants;
 using RestfulBooker.ApiTests.Extensions;
 using RestSharp;
 using TechTalk.SpecFlow;
+
 
 namespace RestfulBooker.ApiTests.Hooks
 {
